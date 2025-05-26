c91fc78367b0ea5bd0d2f23ef98ee81f2f682d3e
CosmicIon <harshnivedan@gmail.com>
2025-05-26 20:15:58 +0530

    updating customer class

--- Customer.java
+++ Customer.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer extends User {
     private boolean hasRequestedCredit = false;
     private boolean isCreditApproved = false;
+    private double balance;
+    private double penaltyAmount = 0.0;
+    private List<Transaction> transactions;
+
 
     public Customer(String name, String email, String password) {
         super(name, email, password);
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
     }
 
     public CreditRequest requestCredit(double amount) {
@@ -17,6 +26,41 @@ public class Customer extends User {
 
     public boolean isCreditApproved() {
         return isCreditApproved;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        addTransaction(new Transaction("Deposit", amount, "Deposited to account"));
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            addTransaction(new Transaction("Withdraw", amount, "Withdrawn from account"));
+            return true;
+        }
+        return false;
+    }
+
+    public void addPenalty(double amount, String reason) {
+        penaltyAmount += amount;
+        addTransaction(new Transaction("Penalty", amount, reason));
+    }
+
+    public double getPenaltyAmount() {
+        return penaltyAmount;
+    }
+
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+    }
+
+    public List<Transaction> getTransactionHistory() {
+        return transactions;
     }
 
     @Override
